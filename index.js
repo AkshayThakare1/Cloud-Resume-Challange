2db344725f4d894f1759ddc68dab7eddf2b3840e
Akshay Thakare <akshay.n.thakare@gmail.com>
2025-06-17 17:19:41 -0500

    Added index.html and index.js files
    added 2 html files

---
+++ index.js
@@ -0,0 +1,14 @@
+const counter = document.querySelector(".counter-number");
+
+async function updateCounter() {
+    try {
+        let response = await fetch("https://gz6tahcmr6pxjbxs5pg762efry0ngjln.lambda-url.us-east-1.on.aws/"); // Replace with your actual Lambda URL
+        let data = await response.json();
+        counter.innerHTML = ` Views: ${data}`;
+    } catch (error) {
+        console.error("Error fetching views:", error);
+        counter.innerHTML = "Views: N/A";
+    }
+}
+
+updateCounter();
