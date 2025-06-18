87d5a28b2c4b68cb79cc1da8e3cd9089672cc73e
Akshay Thakare <akshay.n.thakare@gmail.com>
2025-06-18 10:07:23 -0500

    update file

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ async function updateCounter() {
         counter.innerHTML = ` Views: ${data}`;
     } catch (error) {
         console.error("Error fetching views:", error);
-        counter.innerHTML = "Views: NA/A";
+        counter.innerHTML = "Views: N/A";
     }
 }
 
